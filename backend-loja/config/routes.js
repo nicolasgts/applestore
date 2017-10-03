965d1fb3652288d47e3eb1640deb6a65a26441ae
Nicolas Gabriel <nicolasgabrielt@gmail.com>
2017-10-03 21:00:30 +0200

    GET and PUT working for user/user_id

--- backend-loja/config/routes.js
+++ backend-loja/config/routes.js
@@ -9,8 +9,9 @@ module.exports = function(server){
   var router = express.Router()
  
 
+  // Rotas para /user
     router.route('/user')
-    .get( urlencodeParser, function(req, res){
+      .get( urlencodeParser, function(req, res){
       
             appleStore.User.find(function(err, users){
               if(err)
@@ -38,11 +39,50 @@ module.exports = function(server){
           });
       
           
+        });
+
+        
+    //rotas para user/:id
+
+
+    router.route('/user/:user_id')
+      .get( urlencodeParser, function(req, res){
+      
+            appleStore.User.findById( req.params.user_id, function(err, user){
+              if(err)
+                res.send(err)
+      
+              res.json(user);
+            });
+      
+        })
+     .put(function(req, res) {
+       
+        appleStore.User.findById( req.params.user_id, function(err, user){
+
+          if (err)
+            res.send(err);
+            
+            var user = new appleStore.User();		// create a new instance of the Bear model
+            user.name = req.body.name;  // set the bears name (comes from the request)
+            user.lastName = req.body.lastName;
+            user.email = req.body.email;
+            user.phoneNumber = req.body.phoneNumber;
+            user.date = req.body.date;
+            user.purchases = req.body.purchases;
+
+            user.save(function(err) {
+              if (err)
+                res.send(err);
+        
+              res.json({ message: 'User updated' });
+            });
+          })
+      
+          
         })
 
 
-
-    
 
 
     server.use('/api', router)
